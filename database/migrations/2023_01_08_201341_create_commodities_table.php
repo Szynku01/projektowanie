be2961436bc556474ec4c46619719faf46fe71fc
Szymon <xx>
2023-01-09 12:24:21 +0100

    Poprawiono nazwy w migracjach

--- database/migrations/2023_01_08_201341_create_commodities_table.php
+++ database/migrations/2023_01_08_201341_create_commodities_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('commodities', function (Blueprint $table) {
             $table->string('commodity_code', 4)->unique();
             $table->string('commodity_name',10)->unique();
-            $table->string('unit_shortcut',4); 
-            $table->foreign('unit_shortcut_id')
-            ->references('unit_shortcut')
-            ->on('measurement_units');       
+            $table->string('unit_shortcut'); 
+            $table->foreign('unit_shortcut')
+                ->references('unit_shortcut')
+                ->on('measurement_units');       
             });
     }
 
