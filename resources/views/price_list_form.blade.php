4e6d4609f1bd162dc3cc912b1a801197a54ecf22
Szymon <xx>
2023-01-09 22:01:40 +0100

    Zmodyfikowano formularz cennika

--- resources/views/price_list_form.blade.php
+++ resources/views/price_list_form.blade.php
@@ -11,8 +11,7 @@
             <label class="label">Nazwa towaru</label> <input type="text" name="Nazwa towaru" placeholder="Nazwa towaru"/>
             <label class="label">Kod towaru</label> <input type="text" name="Kod towaru" placeholder="Kod towaru"/>
             <label class="label">Jednostka miary</label> <input type="text" name="Jednostka miary" placeholder="Jednostka miary"/>
-            <label class="label">Cena jednostkowa zakupu</label> <input type="text" name="Cena jednostkowa zakupu" placeholder="Cena jednostkowa zakupu"/>
-            <label class="label">Nowa cena jednostkowa</label> <input type="text" name="Nowa cena jednostkowa" placeholder="Nowa cena jednostkowa"/>
+            <label class="label">Nowa cena jednostkowa</label> <input type="text" name="Cena jednostkowa" placeholder="Nowa cena jednostkowa"/>
 
             <input class="btn-form" type="submit" value="Dodaj">
             <input class="btn-form" type="reset" value="Wróć">
