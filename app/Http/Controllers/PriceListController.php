23d0bf8e2a2e067e68d43463336bd387d5875705
adrian <adrian@localhost>
2023-01-23 01:07:39 +0100

    Stworzono formularz edycji pozycji cennika (TODO: przycisk wróć).

--- app/Http/Controllers/PriceListController.php
+++ app/Http/Controllers/PriceListController.php
@@ -39,16 +39,6 @@ class PriceListController extends Controller
      */
     public function store(Request $request)
     {
-        // return('nawet tu nie wchodzi bo tego nie wyświetla :(');
-        // $price_list = new Price_list;
-
-        // $price_list->price_list_number = count(DB::table('Price_list')->get()) + 1;
-        // $price_list->date_from = $request->date_from;
-        // $price_list->date_to = $request->date_to;
-
-        // $price_list->save();
-        // return redirect('/cenniki');
-
         // Walidacja
         $request->validate([
             'date_from' => 'required',
--- resources/views/price_list_form.blade.php
+++ resources/views/price_list_form.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="wrapper">
     <h1 class="header">Tworznie cennika</h1>
-        <form action="{{ url('savePriceList') }}" method="post">
+        <form action="{{ url('zapiszCennik') }}" method="post">
             @csrf
             <label class="label">Data od</label> <input type="text" name="date_from" placeholder="RRRR-MM-DD"/>
             <label class="label">Data do</label> <input type="text" name="date_to" placeholder="RRRR-MM-DD"/>
